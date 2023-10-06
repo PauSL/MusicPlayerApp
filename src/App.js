29f8767b8bb9b50852c069bfc41a45f09fb9aa61
PauSL <pausaez9218@gmail.com>
2023-10-06 14:56:37 +0200

    Artwork

--- src/App.js
+++ src/App.js
@@ -28,18 +28,20 @@ function App() {
       setSongInfo({...songInfo, currentTime: current, duration, animationPercentage: animation})
      };
 
+     //To Show minis and names in locked phone screens
+
      useEffect(() => {
       if ('mediaSession' in navigator) {
         navigator.mediaSession.metadata = new MediaMetadata({
           title: currentSong.name,
           artist: currentSong.artist,
           artwork: [
-            { src: currentSong.cover, sizes: '96x96', type: 'image/png' },
-            { src: currentSong.cover, sizes: '128x128', type: 'image/png' },
-            { src: currentSong.cover, sizes: '192x192', type: 'image/png' },
-            { src: currentSong.cover, sizes: '256x256', type: 'image/png' },
-            { src: currentSong.cover, sizes: '384x384', type: 'image/png' },
-            { src: currentSong.cover, sizes: '512x512', type: 'image/png' },
+            { src: currentSong.cover, sizes: '96x96', type: 'image/jpg' },
+            { src: currentSong.cover, sizes: '128x128', type: 'image/jpg' },
+            { src: currentSong.cover, sizes: '192x192', type: 'image/jpg' },
+            { src: currentSong.cover, sizes: '256x256', type: 'image/jpg' },
+            { src: currentSong.cover, sizes: '384x384', type: 'image/jpg' },
+            { src: currentSong.cover, sizes: '512x512', type: 'image/jpg' },
           ]
         });
       }
@@ -62,6 +64,8 @@ function App() {
       }
     }, []);
  
+
+    //Auto PLayback//
   
      useEffect(() => {
       if (isPlaying) {
